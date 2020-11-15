c60974fea013c044b3d8215e191392d1cb2a8032
Ligre <ligreman@users.noreply.github.com>
2020-11-15 21:39:50 +0100

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -9,7 +10,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor(translate: TranslateService, public dialog: MatDialog) {
+  constructor(translate: TranslateService, private dialog: MatDialog, private router: Router) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('es');
     // the lang to use, if the lang isn't available, it will use the current loader to get them
@@ -17,6 +18,12 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
   }
 
   openLicenses(): void {
@@ -29,4 +36,13 @@ export class AppComponent implements OnInit {
   templateUrl: 'license-dialog-content.html',
   styleUrls: ['./app.component.css']
 })
-export class LicenseDialogComponent {}
+export class LicenseDialogComponent {
+  constructor(private translate: TranslateService) {
+  }
+
+  getText(key: string): string {
+    let data = this.translate.instant(key);
+    data = data.split(' ');
+    return atob(String.fromCharCode(...data));
+  }
+}
